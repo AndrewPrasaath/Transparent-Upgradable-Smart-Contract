bb5bca256e751655a748258e829aaef9e8227606
Andrew Prasaath <andrewprasaath07@gmail.com>
2023-04-16 22:14:46 +0530

    init commit

---
+++ contracts/BoxV2.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;
+
+contract Box {
+    uint256 internal s_value;
+
+    event ValueChanged(uint256 _value);
+
+    function store(uint256 _value) public {
+        s_value = _value;
+        emit ValueChanged(_value);
+    }
+
+    function increment() public {
+        s_value += 1;
+        emit ValueChanged(s_value);
+    }
+
+    function getValue() public view returns (uint256) {
+        return s_value;
+    }
+
+    function version() public pure returns (uint256) {
+        return 2;
+    }
+}
